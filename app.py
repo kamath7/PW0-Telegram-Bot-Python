2010b31468f28c82de804f5d46e6599db71352d2
kamath7 <adithyakamath96@gmail.com>
2021-09-21 01:03:00 +0530

    setting up dispatcher

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello world')
+import telegram.ext
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+updater = telegram.ext.Updater(TELEGRAM_TOKEN, use_context=True)
+disp = updater.dispatcher
