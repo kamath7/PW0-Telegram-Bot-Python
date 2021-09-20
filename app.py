8da6ed1591b10faff26b4c57989601ed5314d161
kamath7 <adithyakamath96@gmail.com>
2021-09-21 01:30:41 +0530

    getting stock prices

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import telegram.ext
 import os
 from dotenv import load_dotenv
+import requests
+
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
+
 def start(update, context):
     update.message.reply_text("hello! Let's drink water!")
+
+def get_stock_price(stockName):
+
+    r = requests.get("https://kams-stocks.herokuapp.com/infosys").json()
+    return(r['stockPrice'])
+
 
 def help(update, context):
     update.message.reply_text("""
@@ -16,14 +25,26 @@ def help(update, context):
     /content -> some content
     """)
 
+
+def stock(update, context):
+    ticker = context.args[0]
+    price = get_stock_price(ticker)
+    update.message.reply_text(
+        "Current price of {0} is {1}".format(ticker, price,))
+
+
 def content(update, context):
     update.message.reply_text("Let's laugh at  fools hahahahahaha")
 
+
 def contact(update, context):
-    update.message.reply_text("contacct me through email - kamathapp7@gmail.com")
+    update.message.reply_text(
+        "contacct me through email - kamathapp7@gmail.com")
+
 
 def handle_message(update, context):
     update.message.reply_text("You entered {0}".format(update.message.text,))
+
 
 updater = telegram.ext.Updater(TELEGRAM_TOKEN, use_context=True)
 disp = updater.dispatcher
@@ -32,7 +53,9 @@ disp.add_handler(telegram.ext.CommandHandler("start", start))
 disp.add_handler(telegram.ext.CommandHandler("contact", contact))
 disp.add_handler(telegram.ext.CommandHandler("content", content))
 disp.add_handler(telegram.ext.CommandHandler("help", help))
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+disp.add_handler(telegram.ext.CommandHandler("stock", stock))
+disp.add_handler(telegram.ext.MessageHandler(
+    telegram.ext.Filters.text, handle_message))
 
 updater.start_polling()
 updater.idle()
