52711b46f3cb80e17bb634b6cf25f7189a425dbf
kamath7 <adithyakamath96@gmail.com>
2021-09-21 01:08:38 +0530

    helper functions added

--- app.py
+++ app.py
@@ -5,5 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
+def start(update, context):
+    update.message.reply_text("hello! Let's drink water!")
+
+def help(update, context):
+    update.message.reply_text("""
+    You can use the following commands:
+    /start -> greeting
+    /contact -> contact details
+    /content -> some content
+    """)
+
+def content(update, context):
+    update.message.reply_text("Let's laugh at  fools hahahahahaha")
+
+def contact(update, context):
+    update.message.reply_text("contacct me through email - kamathapp7@gmail.com")
+
 updater = telegram.ext.Updater(TELEGRAM_TOKEN, use_context=True)
 disp = updater.dispatcher
